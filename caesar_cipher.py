f895daae94e29b1dea83156a26564b3ae25db435
bell <funkabell@gmail.com>
2016-08-10 20:05:03 +0300

    +user friendly

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,13 +2,33 @@ import string
 
 
 def main():
-    encrypted = encrypt("bass", 2)
-    print encrypted
+    brute = 0
+    action = raw_input("Would you like to encrypt or decrypt? (e/d) ")
+    if action == "d":
+        brute = raw_input("Do you have the key or would you like to brute force? (k/b) ")
 
-    decrypted = decrypt("cbtt", 2)
-    print decrypted
+    if action == "e":
+        str = raw_input("Please enter the string to encrypt : ")
+        key = int(raw_input("Please enter the key : "))
+        print encrypt(str,key)
 
-    decrypt_brute("ifmmp")
+    elif action == "d" and brute == "k":
+        str = raw_input("Please enter the string to decrypt : ")
+        key = int(raw_input("Please enter the key : "))
+        print decrypt(str,key)
+    elif action == "d" and brute == "b":
+        str = raw_input("Please enter the string to decrypt : ")
+        list_brute = decrypt_brute(str)
+        for option in list_brute:
+            print option
+    ##Should return cbtt
+    #encrypted = encrypt("bass", 2)
+    #print encrypted
+    ##Should return bass
+    #decrypted = decrypt("cbtt", 2)
+    #print decrypted
+    ##Should return Hello
+    #decrypt_brute("ifmmp")
 
 
 def encrypt(str, key):
@@ -35,9 +55,10 @@ def decrypt(str, key):
 
 
 def decrypt_brute(str):
+    list_brute = []
     for i in range(1,26):
-        print decrypt(str,i)
-
+        list_brute.append("Key#%s: %s" % (i, decrypt(str,i)))
+    return list_brute
 
 if __name__ == "__main__":
     main()
