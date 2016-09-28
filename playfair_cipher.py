8c6a15e79ee42bbcb19877168efb2e4a2f978c10
bell <funkabell@gmail.com>
2016-09-28 20:04:47 +0300

    pythonic way or whatever

--- playfair_cipher.py
+++ playfair_cipher.py
@@ -1,15 +1,25 @@
+import string
+
+
 def main():
-    text = list('go to new york')
-    password = 'fiveabcdghklmnopqrstuwxyz'
+    key = 'five'
+    password = adjust_password(key)
+
+    text = 'go to new york'
     print encrypt_playfair(text, password)
 
-    text = list('hnztsgxzmtmu')
+    text = 'hnztsgxzmtmu'
     print decrypt_playfair(text, password)
 
 
-def encrypt_playfair(text, password):
+def encrypt_playfair(text_string, password):
+    print text_string
     encrypted_str = ''
-    check_validity(text)  # deletes spaces and adjusts string length
+    # deletes spaces and adjusts string length
+    text = adjust_text(text_string)
+    print text
+    text = list(text)
+    print text
 
     for i in xrange(0, len(text), 2):
         first_letter = text[i]
@@ -20,10 +30,10 @@ def encrypt_playfair(text, password):
 
         first_position = password.find(first_letter)
         second_position = password.find(second_letter)
-        if same_row(first_position, second_position):
-            first_result, second_result = samerow_position(first_position, second_position)
-        elif same_column(first_position, second_position):
-            first_result, second_result = samecolumn_position(first_position, second_position)
+        if is_same_row(first_position, second_position):
+            first_result, second_result = change_samerow_position(first_position, second_position)
+        elif is_same_column(first_position, second_position):
+            first_result, second_result = change_samecolumn_position(first_position, second_position)
         else:
             first_result, second_result = new_position(first_position, second_position)
         encrypted_str = encrypted_str + password[first_result] + password[second_result]
@@ -31,7 +41,8 @@ def encrypt_playfair(text, password):
     return encrypted_str
 
 
-def decrypt_playfair(text, password):
+def decrypt_playfair(text_string, password):
+    text = list(text_string)
     decrypted_str = ''
 
     for i in xrange(0, len(text), 2):  # runs 6 times
@@ -43,18 +54,21 @@ def decrypt_playfair(text, password):
 
         first_position = password.find(first_letter)
         second_position = password.find(second_letter)
-        if same_row(first_position, second_position):
+
+        if is_same_row(first_position, second_position):
             first_result, second_result = re_samerow_position(first_position, second_position)
-        elif same_column(first_position, second_position):
+        elif is_same_column(first_position, second_position):
             first_result, second_result = re_samecolumn_position(first_position, second_position)
         else:
             first_result, second_result = new_position(first_position, second_position)
+
         decrypted_str = decrypted_str + password[first_result] + password[second_result]
 
     return decrypted_str
 
 
-def same_row(first, second):            #whether the letters are in the same row
+#whether the letters are in the same row
+def is_same_row(first, second):
     first_row = first / 5
     second_row = second / 5
     if first_row == second_row:
@@ -63,7 +77,8 @@ def same_row(first, second):            #whether the letters are in the same row
         return False
 
 
-def same_column(first, second):         #whether the letters are in the same column
+#whether the letters are in the same column
+def is_same_column(first, second):
     first_column = first % 5
     second_column = second % 5
     if first_column == second_column:
@@ -72,24 +87,20 @@ def same_column(first, second):         #whether the letters are in the same col
         return False
 
 
-def samerow_position(first, second):    #get new position if letters are in the same row
-    first = (first + 1) % 25
-    second = (second + 1) % 25
-
-    return first, second
+#get new position if letters are in the same row
+def change_samerow_position(first, second):
+    return return_placement(first + 1, second + 1)
 
 
-def samecolumn_position(first, second): #get new position if letters are in the same column
-    first = (first + 5) % 25
-    second = (second + 5) % 25
-
-    return first, second
+#get new position if letters are in the same column
+def change_samecolumn_position(first, second):
+    return return_placement(first + 5, second + 5)
 
 
-def new_position(first, second):        #get new position if letters are not in the same row or column
+#get new position if letters are not in the same row or column
+def new_position(first, second):
     first_row = first / 5
     first_column = first % 5
-    # print first_column
     second_row = second / 5
     second_column = second % 5
 
@@ -104,44 +115,57 @@ def new_position(first, second):        #get new position if letters are not in 
     return first_result, second_result
 
 
-def re_samerow_position(first, second): #get original position if letters are in the same column
-    first = (first - 1) % 25
-    second = (second - 1) % 25
+#get original position if letters are in the same column
+def re_samerow_position(first, second):
+    return return_placement(first - 1, second - 1)
+
+
+#get original position if letters are not in the same column or row
+def re_samecolumn_position(first, second):
+    return return_placement(first - 5,second - 5)
+
+
+def return_placement(desired_first, desired_second):
+    first = desired_first % 25
+    second = desired_second % 25
 
     if first < 0:
-        first = 24
+        first += 25
     if second < 0:
-        second = 24
+        second += 25
     return first, second
 
 
-def re_samecolumn_position(first, second):  #get original position if letters are not in the same column or row
-    first = (first - 5) % 25
-    second = (second - 5) % 25
+#adjusts length
+def adjust_text(text):
+    text = text.replace(' ', '')
 
-    if first < 0:
-        first = 25 + first
-    if second < 0:
-        second = 25 + second
-    return first, second
-
-
-def check_validity(text):       #adjusts length
-    i = -1
-    while (len(text) != i):
-        i = i + 1
-        if i == len(text):
-            break
-        if text[i] == ' ':
-            del text[i]
-    for j in xrange(len(text) - 1):
-        if (text[j] == text[j + 1]) and (text[j] != 'x'):
-            text[j + 1] = 'x'
+    for i in xrange(len(text) - 1):
+        if (text[i] == text[i + 1]) and (text[i] != 'x'):
+            text[i + 1].replace('x')
 
     if len(text) % 2 != 0:
-        text = text.append('x')
+        text += 'x'
 
-    return (text)
+    return text
+
+
+def adjust_password(password):
+    add_pass = string.lowercase
+    for letter in password:
+        if add_pass.find(letter)>0:
+            position = add_pass.find(letter)
+            add_pass = add_pass[:position] + add_pass[position+1:]
+
+        if 'j' in add_pass and 'i' not in add_pass:
+            add_pass = ''.join(add_pass.split('j'))
+        elif 'j' not in add_pass and 'i' in add_pass:
+            add_pass = ''.join(add_pass.split('i'))
+
+    if 'i' and 'j' in add_pass:
+        add_pass = ''.join(add_pass.split('j'))
+    return password + add_pass
+
 
 
 if __name__ == "__main__":
